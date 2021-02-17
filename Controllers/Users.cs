9aafd2084dcccf20f78d1fbf454c90fba043977c
ssyadwad <sagarmfyadwad@gmail.com>
2021-02-17 18:21:29 +0530

    End of section 2

---
+++ Controllers/Users.cs
@@ -0,0 +1,26 @@
+﻿using DatingWebAppScratch.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using DatingWebAppScratch.Models;
+using System.Threading.Tasks;
+
+namespace DatingWebAppScratch.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Users:ControllerBase
+    {
+        private AppDbContext _dbContext { get; set; }
+        public Users(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AppUser> GetUsers(int id)
+        {
+            return  await _dbContext.Users.FindAsync(id);
+        }
+    }
+}
